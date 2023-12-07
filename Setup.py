c9f26cc4514db633ba4a77af68fd10de97e182ac
gurpej singh <68013567+gurpejsingh13@users.noreply.github.com>
2023-12-07 22:07:03 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='punjabi_stopwords',
     version='0.1.0',
-    packages=find_packages(),
+    py_modules=['punjabi_stopwords'], 
     description='A Python library for Punjabi language stopwords.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Gurpej Singh',
     author_email='gurpejsingh462@gmail.com',
-    url='https://github.com/gurpejsingh13/Punjabi_Stopwords.git,
+    url='https://github.com/gurpejsingh13/Punjabi_Stopwords.git', 
     license='MIT',
-    keywords=['Stopwords','punjabi stopwords nlp','Punjabi Language Processing ','Natural Language Processing'],
+    keywords=['stopwords', 'punjabi', 'nlp', 'punjabi language', 'natural language processing'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -20,9 +20,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.9',  
     ],
-    
 )
 
 
