caa26404023a2fbb177940a99f82d0b102dc401b
gurpej singh <68013567+gurpejsingh13@users.noreply.github.com>
2023-12-07 21:30:43 +0530

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='punjabi_stopwords',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A Python library for Punjabi language stopwords.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Gurpej Singh',
+    author_email='gurpejsingh462@gmail.com',
+    url='https://github.com/gurpejsingh13/Punjabi_Stopwords.git,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='Stopwords','punjabi stopwords nlp,'Punjabi Language Processing ','Punjabi Preprocessing ',
+)
+
+
+
